7606a954fd441610dd3be99f8da8d348ead53407
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-05-25 15:47:50 +0900

    role-base login with csrf token POST

--- src/main/java/com/example/security/WebSecurityConfig.java
+++ src/main/java/com/example/security/WebSecurityConfig.java
@@ -1,45 +1,45 @@
 package com.example.security;
 
+import com.example.security.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/","/home").permitAll()
-                    .anyRequest().authenticated()
+                    .antMatchers("/signUp","/login","/user").permitAll()
+                    .antMatchers("/","home").hasRole("USER")
+                    .antMatchers("/admin").hasRole("ADMIN")
                     .and()
                 .formLogin()
                     .loginPage("/login")
-                    .permitAll()
-                    .defaultSuccessUrl("/hello")//로그인 성공 후 리다이렉트 주소
+                    .defaultSuccessUrl("/")//로그인 성공 후 리다이렉트 주소
                     .and()
                 .logout()
+                    .logoutSuccessUrl("/login")
                     .invalidateHttpSession(true)//세션 날리기
-                    .logoutSuccessUrl("/home")
                     .permitAll();
-
     }
 
-    @Bean
     @Override
-    protected UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
